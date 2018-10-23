8701dbfbc5416843212033c4c6adabb23716af9f
Michael Kochell <michael@iris-works.com>
2018-10-22 23:30:20 -0400

    new bucket cloud.jaam.app

--- app/models/audio_file.rb
+++ app/models/audio_file.rb
@@ -3,6 +3,15 @@ class AudioFile < ApplicationRecord
   has_one_attached :attached_file
 
   def url
-    url_for(attached_file) if attached_file.attached?
+    return nil unless attached_file.attached?
+    return file_storage_host_url if ENV.fetch('FILE_STORAGE_HOST').present?
+
+    url_for(attached_file)
+  end
+
+  private
+
+  def file_storage_host_url
+    "#{ENV.fetch('FILE_STORAGE_HOST')}/#{attached_file.key}"
   end
 end
