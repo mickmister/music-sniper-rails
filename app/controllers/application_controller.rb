d3f10d6a9dbaa94aeb4ea0e61fdec8f08bf7e9e7
Michael Kochell <michael@iris-works.com>
2018-10-22 22:01:36 -0400

    actually fix cors this time

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::API
   before_action :force_json
+  before_action :set_cors
   # skip_before_action :verify_authenticity_token
 
   def force_json
      request.format = :json
+  end
+
+  def set_cors
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
