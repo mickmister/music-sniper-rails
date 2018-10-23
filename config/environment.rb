40be163fe025d5f2acd9ad5985cfea564eef8e57
Michael Kochell <michael@iris-works.com>
2018-10-22 20:25:38 -0400

    change asset host

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-MusicSniperRails::Application.default_url_options = {host: 'localhost:1234'}
+MusicSniperRails::Application.default_url_options = {host: ENV.fetch('ASSET_HOST')}
