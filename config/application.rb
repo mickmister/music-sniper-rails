fb8628a32a5f2cf02a06e6e64c6ddb305f4f8e75
Michael Kochell <michael@iris-works.com>
2019-01-13 18:04:32 -0500

    module load

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module MusicSniperRails
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.autoload_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
