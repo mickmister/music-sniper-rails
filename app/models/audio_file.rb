01b130f2829c2387db2351d5ad8bdeb466eac86d
mickmister <mjkochell@gmail.com>
2019-07-21 06:13:11 -0400

    add extension to file url

--- app/models/audio_file.rb
+++ app/models/audio_file.rb
@@ -4,6 +4,6 @@ class AudioFile < ApplicationRecord
   has_one_attached :attached_file
 
   def url
-    calculate_url(attached_file)
+    "#{calculate_url(attached_file)}.#{attached_file.filename.extension}"
   end
 end
